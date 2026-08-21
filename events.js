5c98b3a7ef0ba6f8b95ea1ba8db9e7d664576be6
nishhhi11 <nishi@NISHIs-MacBook-Air.local>
2026-08-21 12:01:24 +0530

    events

---
+++ events.js
@@ -0,0 +1,14 @@
+//events are like click , mouseover, mouseout, keypress, keydown, keyup, load, unload, resize, scroll, select, change, submit, reset, focus, blur etc.
+
+//event is done using addEventListener() 
+//in which AddEventListener("event", function , option) (option could be true or false but bydefault is false) (function is the event listener)
+//event -> click, submit, change, keyword, load, mouseover
+
+//in one id we can use multiple event listeners 
+
+//event propagation 
+//event capturing phase -> false value [parent to child]
+//event bubbling phase -> true value [child to parent]
+//event target phase
+//stop propagation vs stop immediate propagation
+
